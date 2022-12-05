841b26f82c7fa89481070c0aa79ae2c7db1506b1
Gabriel <grodriguez@emergya.com>
2022-12-05 15:36:15 +0100

    feat(): Create Nodejs and Browser Javascript client for Centrifugo using Websockets

---
+++ index.js
@@ -0,0 +1,36 @@
+const {Centrifuge} = require('centrifuge');
+const express = require('express');
+const WebSocket = require('ws');
+
+var centrifuge = new Centrifuge('ws://localhost:8000/connection/websocket', {
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjMiLCJleHAiOjE2NzA4NTQ5ODIsImlhdCI6MTY3MDI1MDE4Mn0.OCUSxQ6ebIPqQk-RywynvQWEofYDF0TjBVhyBWNEf3Q',
+    websocket: WebSocket
+})
+
+const sub = centrifuge.newSubscription('channel');
+
+sub.on('publication', function(ctx) {
+    console.log(ctx.data);
+});
+
+sub.subscribe();
+
+centrifuge.on('connected', () => {
+  console.log('connected');
+})
+
+centrifuge.on('connecting', () => {
+  console.log('connecting');
+})
+
+centrifuge.on('disconnected', () => {
+  console.log('disconnected');
+})
+
+centrifuge.connect();
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server ready');
+})
